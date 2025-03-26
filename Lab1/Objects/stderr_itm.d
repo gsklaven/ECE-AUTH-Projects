7b1c437120be33a05793983dada410ea2127d586
gsklaven <sklavenitisgeorge21@gmail.com>
2025-03-26 20:39:56 +0200

    remove unecessary files

--- Lab1/Objects/stderr_itm.d
+++
@@ -1,8 +0,0 @@
-./objects/stderr_itm.o: \
-  C:\Users\sklav\AppData\Local\Arm\Packs\ARM\CMSIS-Compiler\2.1.0\source\stderr_itm.c \
-  C:\Users\sklav\AppData\Local\Arm\Packs\ARM\CMSIS-Compiler\2.1.0\include\retarget_stderr.h \
-  RTE\_Target_1\RTE_Components.h \
-  C:\Users\sklav\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
-  C:\Users\sklav\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f411xe.h \
-  C:\Users\sklav\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Users\sklav\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
