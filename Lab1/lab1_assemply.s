e5876fda2dd7ddadf8e4b360ce6ad8f161f5473f
gsklaven <sklavenitisgeorge21@gmail.com>
2025-03-27 17:42:31 +0200

    add ascii part

--- Lab1/lab1_assemply.s
+++ Lab1/lab1_assemply.s
@@ -5,15 +5,15 @@
 hashing:
     .fnstart
     mov r1, #0       // r1 = 0, counter (string length)
-	mov r3, r0
+	mov r2, r0
 loop0:
-    ldrb r2, [r0], #1 // Load byte from r0, increment r0 by 1
-    cmp r2, #0        // Check if null terminator
+    ldrb r3, [r0], #1 // Load byte from r0, increment r0 by 1
+    cmp r3, #0        // Check if null terminator
     beq hashing_start // If null, go to hashing_start
     add r1, r1, #1    // Increase counter (string length)
     b loop0            // Repeat loop
 hashing_start:
-    mov r4, r1        // Move hash value to return global variable (r4)
+    mov r0, r1        // Move hash value to return register (r0)
 	bx lr             // Return
 	.fnend
 	
@@ -24,18 +24,30 @@ hashing_start:
 .type asciicompare,%function
 asciicompare:
     .fnstart
-	mov r0, #'A'
+	push {lr} //
+	push {r0, r1} //push the values before hashing changes them
+	bl hashing //Call the function
+	mov r4, r0  //Move hashing output to r4
+	pop {r0, r1} //restore r0, r1
+	mov r3, r0 //Move string address to r3
 loop1:
-	cmp r3, r0
-	beq asciiuppercase
-	add r0, r0, #1
-	b loop1
+	ldrb r2, [r3], #1 
+    cmp r2, #0       
+    beq end_compare
+
+    cmp r2, #'A'     
+    blt loop1         
+
+    cmp r2, #'Z'      
+    bgt loop1         
+
+    add r0, r2, r2    
+    add r4, r4, r0    
+    b loop1        
+end_compare:
+    mov r0, r4       
+    pop {lr}   
+    bx lr  
+    .fnend
 	
-asciiuppercase:
-	mov r1, #2
-	mul r0, r0, r1
-	bl hashing //Call function
-	add r4, r4, r0
-	bx lr
-	.fnend
 	
--- Lab1/main.c
+++ Lab1/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 extern int hashing(const char * string);
+extern int asciicompare(const char * string);
 
 int main() {
-    char* string = "A01";
+    char* string = "A0345";
     
-    printf("The lenght of the string is : %d", hashing(string));
+    printf("The lenght of the string is : %d", asciicompare(string));
 
     return 0;
 }
