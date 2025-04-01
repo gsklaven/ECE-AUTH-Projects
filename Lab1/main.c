4683d31dc9ca399c7a7b5a7bef2ba0bb55735b23
gsklaven <sklavenitisgeorge21@gmail.com>
2025-04-01 20:33:36 +0300

    add all functions

--- Lab1/main.c
+++ Lab1/main.c
@@ -3,17 +3,30 @@
 extern int hashing(const char * string);
 extern int modulowith7(const char * string);
 extern int fibonacci(const char * string);
-extern int result;
+extern int addhash(const char * string);
+extern int asciicompare(const char * string);
+extern int numberchange(const char * string);
+
+extern int result0;
+extern int result1;
+extern int result2;
 
 int main() {
-    char* string = "A0345a";
+    char* string = "A9b3";
 	
     printf("The length of the string is : %d \n", hashing(string));
-		printf("The length of the string is : %d \n", modulowith7(string));
-    printf("The length of the string is : %d \n", fibonacci(string));
+		printf("The ascii result is : %d \n", asciicompare(string));
+		printf("The numbers changed give: %d \n", numberchange(string));
+		printf("The total hash is : %d \n", addhash(string));
+		printf("The modulo with 7 is : %d \n", modulowith7(string));
+    printf("The fibonacci of the number is : %d \n", fibonacci(string));
 	
+		numberchange(string);
+		printf("Hash result is: %d\n", result0);
+		modulowith7(string);
+		printf("Modulo result is: %d\n", result1);
 		fibonacci(string);
-    printf("Fibonacci result: %d\n", result);
+    printf("Fibonacci result: %d\n", result2);
 	
     return 0;
 }
